9ec7a2673a7bb99c997ab1dc46ad9e87e4eca4aa
Marin Procureur <marin.procureur@gmail.com>
2017-03-10 14:53:11 +0100

    Add basic chords triads (major, minor, dim, aug) & tests

--- lib/musix/chord.ex
+++ lib/musix/chord.ex
@@ -1,18 +1,105 @@
 defmodule Musix.Chord do
   use Musix.Note
 
+  @chords %{"major" => "major","minor" => "minor","augmented" => "aug","diminished" => "dim"}
+
+  def get_chords do
+    @chords
+  end
+
   ## a major triad is composed by a root note, a major third and a perfect fifth
   def get_major_triad(root) do
     case get_major_third(root) do
       {:ok, third} ->
         case get_perfect_fifth(root) do
           {:ok, fifth} ->
-            {:ok, [root, third, fifth]}
+            {
+              :ok,
+              [root, third, fifth]
+            }
           {:error, message} ->
-            {:error, "Error while obtaining the fifth : " <> message}
+            {
+              :error,
+              "Error while obtaining the fifth : " <> message
+            }
         end
       {:error, message} ->
-        {:error, "Error while obtaining the fifth : " <> message}
+        {
+          :error,
+          "Error while obtaining the fifth : " <> message
+        }
+    end
+  end
+
+  ## a minor triad is composed by a root note, a minor third and a perfect fifth
+  def get_minor_triad(root) do
+    case get_minor_third(root) do
+      {:ok, third} ->
+        case get_perfect_fifth(root) do
+          {:ok, fifth} ->
+            {
+              :ok,
+              [root, third, fifth]
+            }
+          {:error, message} ->
+            {
+              :error,
+              "Error while obtaining the fifth : " <> message
+            }
+        end
+      {:error, message} ->
+        {
+          :error,
+          "Error while obtaining the fifth : " <> message
+        }
+    end
+  end
+
+  ## an augmented triad is composed by a root note, a major third and an augmented fifth
+  def get_augmented_triad(root) do
+    case get_major_third(root) do
+      {:ok, third} ->
+        case get_augmented_fifth(root) do
+          {:ok, fifth} ->
+            {
+              :ok,
+              [root, third, fifth]
+            }
+          {:error, message} ->
+            {
+              :error,
+              "Error while obtaining the fifth : " <> message
+            }
+        end
+      {:error, message} ->
+        {
+          :error,
+          "Error while obtaining the fifth : " <> message
+        }
+    end
+  end
+
+  ## a diminished triad is composed by a root note, a major third and a diminished fifth
+  def get_diminished_triad(root) do
+    case get_minor_third(root) do
+      {:ok, third} ->
+        case get_diminished_fifth(root) do
+          {:ok, fifth} ->
+            {
+              :ok,
+              [root, third, fifth]
+            }
+          {:error, message} ->
+            {
+              :error,
+              "Error while obtaining the fifth : " <> message
+            }
+        end
+      {:error, message} ->
+        {
+          :error,
+          "Error while obtaining the fifth : " <> message
+        }
     end
   end
 
--- test/chord_test.exs
+++ test/chord_test.exs
@@ -2,10 +2,41 @@ defmodule Musix.ChordTest do
   use ExUnit.Case
   use Musix.Chord
 
+  test "get all chords" do
+    case get_chords() do
+      x ->
+        assert(is_map(%{"major" => "major","minor" => "minor","augmented" => "aug","diminished" => "dim"}))
+    end
+  end
+
   test "get a major triad" do
     case get_major_triad("C") do
       {atom, chord} ->
         assert(["C","E","G"] === chord)
+        assert(atom == :ok)
+    end
+  end
+
+  test "get a minor triad" do
+    case get_minor_triad("C") do
+      {atom, chord} ->
+        assert(["C","Eb","G"] === chord)
+        assert(atom == :ok)
+    end
+  end
+
+  test "get an augmented triad" do
+    case get_augmented_triad("G") do
+      {atom, chord} ->
+        assert(["G","B","Ds"] === chord)
+        assert(atom == :ok)
+    end
+  end
+
+  test "get a diminished triad" do
+    case get_diminished_triad("G") do
+      {atom, chord} ->
+        assert(["G","Bb","Db"] === chord)
         assert(atom == :ok)
     end
   end
